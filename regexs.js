5c501747de22b2e9b86488572af75d77c99078bc
Guido <38738725+guidomodarelli@users.noreply.github.com>
2021-03-15 14:42:18 -0300

    Update regexs.js

--- regexs.js
+++ regexs.js
@@ -3,3 +3,4 @@ email = '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'
 // https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
 url = '^(((https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$'
 password = '(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'
+instagram = '^(?!.*[.][.])(?!^[.])(?!.*[.]$)(?!^[0-9_]+$)[a-z0-9_.]{3,50}$'
